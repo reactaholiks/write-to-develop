44ec5374de22b83d17cd7d6b02247401b3fd4693
santosh <santosh.k@techouts.com>
2024-03-13 13:25:43 +0530

    issue fixed

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,50 +1,42 @@
-// import React from "react"
+import React from "react"
 
-// export const Header = (props) => {
-//   console.log("props:", props)
-//   const { SetItems } = props
+export const Header = (props) => {
+  console.log("props:", props)
+  const { SetItems } = props
 
-//   return (
-//     <>
-//       <div
-//         style={{
-//           backgroundColor: "#ec5353",
-//           height: "7vw",
-//           display: "flex",
-//           justifyContent: "space-between",
-//           alignItems: "center",
-//         }}>
-//         <h1 style={{ textAlign: "left", margin: "0px", padding: "35px" }}>
-//           {/* {SetItems()} */}
-//         </h1>
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#ec5353",
+          height: "7vw",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}>
+        <h1 style={{ textAlign: "left", margin: "0px", padding: "35px" }}>
+          {/* {SetItems()} */}
+        </h1>
 
-//         <div style={{ display: "flex", gap: "10vw" }}>
-//           {SetItems?.items?.map((item) => {
-//             return (
-//               <>
-//                 <p>{item.title}</p>
-//               </>
-//             )
-//           })}
-//         </div>
-//         <p style={{ fontSize: "30px", fontWeight: 600, paddingRight: "40px" }}>
-//           |||
-//         </p>
-//       </div>
-//     </>
-//   )
-// }
-
-
-import React from "react";
-
-class Header extends React.Component{
-  render(){
-    return (
-      <>
-        <h1>{this.props.title}</h1>
-      </>
-    )
-  }
+        <div style={{ display: "flex", gap: "10vw" }}>
+          {SetItems?.items?.map((item) => {
+            return (
+              <>
+                <p>{item.title}</p>
+              </>
+            )
+          })}
+        </div>
+        <p style={{ fontSize: "30px", fontWeight: 600, paddingRight: "40px" }}>
+          |||
+        </p>
+      </div>
+    </>
+  )
 }
+
 export default Header
+
+// import React from "react";
+
+
