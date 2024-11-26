116bd960be7133244e4f8852fdbb985d74756a36
Santhosh.k <santosh.k@techouts.com>
2024-11-27 00:14:18 +0530

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static export
-  basePath: "/repo-name", // Replace 'repo-name' with your GitHub repository name
+  basePath: "/write-to-develop", // Replace 'repo-name' with your GitHub repository name
   images: {
     unoptimized: true, // Required if you're using `next/image`
   },
